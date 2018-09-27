77b697757459138613f2187b59af853a8bb3caf2
Josh <og.ginger@gmail.com>
2018-09-27 21:50:15 +0000

    Optimized bTestAllAsync.

--- build.js
+++ build.js
@@ -3,9 +3,29 @@
 */ 
 ({
     paths: {
-	"log": "lib/log.min"
+	"log": "lib/log.min",
+	"jquery": "empty:",
+	"underscore": "lib/underscore.min",
+	"backbone": "lib/backbone.min",
+	"rsvp": "lib/rsvp.min",
+	"promise": "lib/promise.min",
+	"AsyncIterator": "lib/async_iterator.min",
+	"async_every": "lib/async_every.min",
+	"validate": "lib/validate.min",
+	"AsyncValidate": "lib/async_validate.min"
     },
-    exclude: [ "log" ],
+    exclude: [ 
+	"log", 
+	"jquery", 
+	"underscore", 
+	"backbone", 
+	"rsvp", 
+	"promise", 
+	"AsyncIterator", 
+	"async_every",
+	"validate",
+	"AsyncValidate" 
+    ],
     name: "bTestAllAsync",
     out: "test_all_async.min.js"
 })
